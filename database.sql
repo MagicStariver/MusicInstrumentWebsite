56f38174edd324a7b561d6b3eb43bd78cee40c22
MagicStariver <kychoy1216@gmail.com>
2025-12-04 17:45:42 +0800

    let function.php link to api

--- database.sql
+++ database.sql
@@ -23,7 +23,7 @@ CREATE TABLE users (
 CREATE TABLE products (
     id INT AUTO_INCREMENT PRIMARY KEY,
     product_name VARCHAR(255) NOT NULL,
-    description TEXT,
+    description TEXT,   
     price DECIMAL(10,2) NOT NULL,
     category VARCHAR(100),
     image_source VARCHAR(500),
